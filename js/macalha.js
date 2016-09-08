bd75acbc6275dc41b66ccd3e225c11a106c2961f
macalha() <macalha@gmail.com>
2016-09-08 16:02:07 -0300

    Add files via upload

--- js/macalha.js
+++ js/macalha.js
@@ -1,1 +1,20 @@
+!function(win,doc,html){
+// JavaScript
+	html.className = html.className.replace(/\bno-js\b/,'js');
 
+// Fonts
+	+function(){
+		var fn = function(){
+			html.className += ' font-loaded';
+		};
+		if(!!doc.fonts) {
+			var fonts = ['Open Sans','Open Sans Condensed','Source Code Pro'];
+			for(var i = 0, j = fonts.length; i < j; i++) doc.fonts.load('1em ' + fonts[i]);
+			doc.fonts.ready.then(fn);
+		} else {
+			if(win.addEventListener) win.addEventListener('load',fn,false);
+			else win.attachEvent('onload',fn);
+		}
+	}();
+
+}(window,document,document.documentElement || document.getElementsByTagName('html')[0]);
